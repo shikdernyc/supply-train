e2eabc8ec85635f3aa283f8fe07f8292b7ee8784
Asif Shikder <shikdernyc@gmail.com>
2020-03-29 09:10:33 -0400

    added starter for data formatter

---
+++ scripts/update_initial_data/data_formatter.js
@@ -0,0 +1,22 @@
+const covidApi = require('covid19-api');
+const util = require('util');
+
+async function getStatesICUBeds() {
+  const capacity = await covidApi.getCapacityInfoUSHealthFacilities();
+  return capacity;
+}
+
+async function run() {
+  const result = await getStatesICUBeds();
+
+  console.log(util.inspect(result, { showHidden: false, depth: null }));
+
+  // alternative shortcut
+  console.log(util.inspect(result, false, null, true /* enable colors */));
+}
+
+run();
+
+module.exports = {
+  getStatesICUBeds,
+};
