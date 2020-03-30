968152119c42e56b6f3c493842806ae33049a9ae
Tomasz Wiercioch <47215529+tomawash@users.noreply.github.com>
2020-03-29 20:50:15 -0400

    Completed getICUBeds function.

--- scripts/update_initial_data/data_formatter.js
+++ scripts/update_initial_data/data_formatter.js
@@ -2,8 +2,45 @@ const covidApi = require('covid19-api');
 const util = require('util');
 
 async function getStatesICUBeds() {
-  const capacity = await covidApi.getCapacityInfoUSHealthFacilities();
-  return capacity;
+
+  //const capacity = await covidApi.getCapacityInfoUSHealthFacilities();
+  const bedsData = await covidApi.getCapacityInfoUSHealthFacilities();
+  //console.log(bedsData[0][0].table);
+
+  var hospitals = bedsData[0][0].table;
+  var stateICUBeds = {};
+
+  //stateICUBeds["one"] = 1;
+  //var statesNumb = 0;
+
+  for(i = 0; i < hospitals.length; i++)
+  {
+    var fascility = hospitals[i];
+    var state = fascility.State;
+    var bedsNumber = fascility.StaffedICUBeds;
+    bedsNumber = bedsNumber.replace(/['"]+/g, '');
+    
+    if(bedsNumber === "")
+    {
+      bedsNumber = 0;
+    }
+    else
+    {
+      bedsNumber = parseInt(bedsNumber)||0;
+    }
+
+    if(!stateICUBeds.hasOwnProperty(state))
+    {
+      stateICUBeds[state] = bedsNumber;
+    }
+    else
+    {
+      stateICUBeds[state] = stateICUBeds[state] + bedsNumber;
+    }
+  }
+
+  return stateICUBeds;
+  //return capacity;
 }
 
 async function run() {
@@ -12,7 +49,7 @@ async function run() {
   console.log(util.inspect(result, { showHidden: false, depth: null }));
 
   // alternative shortcut
-  console.log(util.inspect(result, false, null, true /* enable colors */));
+  //console.log(util.inspect(result, false, null, true /* enable colors */));
 
   console.log('Made a change');
 }
