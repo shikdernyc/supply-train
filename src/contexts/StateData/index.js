303624445ee6b215ce363bf373e9e60f05c41db8
Asif Shikder <shikdernyc@gmail.com>
2020-03-31 13:54:29 -0400

    Added OrderManager. Creates order as needed

--- src/contexts/StateData/index.js
+++ src/contexts/StateData/index.js
@@ -1,9 +1,73 @@
-import React, { createContext, useState, useEffect } from 'react';
+/* eslint-disable no-restricted-syntax */
+import React, {
+  createContext, useState, useEffect, useMemo,
+} from 'react';
 import useSettings from 'hooks/useSettings';
 import { getAllStateCasesData } from 'services/covid19';
 import PropTypes from 'prop-types';
 import { getRandomInt } from 'utils/math/getRandomInt';
 import state from 'constants/state';
+import { orderItems } from 'constants/order';
+import { getStatesICUBedData } from 'services/database';
+// eslint-disable-next-line
+import useOrderActions from 'hooks/useOrderActions';
+import { useAllIncomingOrders } from 'hooks/useOrders';
+
+const stateToKey = {};
+const stateList = [];
+Object.keys(state).forEach((stateKey) => {
+  const stateName = state[stateKey];
+  stateToKey[stateName] = stateKey;
+  stateList.push(stateName);
+});
+
+
+async function getInitialData(critToTotal, ventToIcu) {
+  const icuBeds = await getStatesICUBedData();
+  const stateData = await getAllStateCasesData();
+  const [icuMin, icuMax] = ventToIcu;
+  const [critMin, critMax] = critToTotal;
+
+  const totalCases = {};
+  const newCases = {};
+  const activeCases = {};
+  const criticalCases = {};
+  const ventilators = {};
+  const incomingVentilators = {};
+
+  stateList.forEach((stateName) => {
+    const data = stateData[stateName];
+    totalCases[stateName] = data.totalCases;
+    newCases[stateName] = data.newCases;
+    activeCases[stateName] = data.activeCases;
+    criticalCases[stateName] = parseInt(
+      data.totalCases * getRandomInt(critMax, critMin),
+    );
+    const ventilator = getRandomInt(parseInt(icuBeds[stateName] * icuMax), parseInt(icuBeds[stateName] * icuMin));
+    ventilators[stateName] = ventilator;
+  });
+
+  return {
+    totalCases,
+    newCases,
+    activeCases,
+    criticalCases,
+    ventilators,
+    incomingVentilators,
+  };
+}
+
+function BuildOrder(fromStateName, toStateName, quantity) {
+  return ({
+    to: stateToKey[toStateName],
+    from: stateToKey[fromStateName],
+    quantity,
+    item: orderItems.VENTILATOR,
+    expected_by: [new Date(), 15],
+  });
+}
+
+// =================== CONTEXT ===================
 
 const StateDataContext = createContext();
 
@@ -14,41 +78,61 @@ export function StateDataProvider({ children }) {
   const [criticalCases, setCriticalCases] = useState({});
   const [activeCases, setActiveCases] = useState({});
   const [ventilators, setVentilators] = useState({});
-  const [incomingVentilators, setIncomingVentilators] = useState({});
+  const [criticalCaseProjected, setCriticalCaseProjected] = useState({});
+  const [statesWithExcess, setStatesWithExcess] = useState(new Set());
+  const [statesInCritical, setStatesInCritical] = useState(new Set());
+
+  const incomingVentilators = useAllIncomingOrders();
+
+  const stateIncomingVentilators = (stateName) => incomingVentilators[stateName] || 0;
+
+
   const [loading, setLoading] = useState(true);
-  const settings = useSettings();
+  const {
+    ventilatorToIcuPercentage,
+    criticalToTotalPercentage,
+    projectedTimeline,
+  } = useSettings();
+  const { addBulkOrders } = useOrderActions();
+
+  const calculateProjectedCritical = (currentCritical, currentActive) => {
+    // debugger;
+    const rate = (currentCritical / currentActive) + 1;
+    let projected = currentCritical;
+    for (let i = 0; i < projectedTimeline; i++) {
+      projected *= rate;
+    }
+    return parseInt(projected);
+  };
 
   useEffect(() => {
     async function updateInitialStates() {
-      const { criticalToTotalPercentage } = settings;
-      const stateData = await getAllStateCasesData();
-      // INIT COLUMNS
-      const updatedTotal = {};
-      const updatedNewCases = {};
-      const updatedActiveCases = {};
-      const updatedCriticalCases = {};
-      const updatedVentilators = {};
-      const updatedIncomingVentilators = {};
-      // POPULATE EACH COLUMN
-      const states = Object.values(state);
-      states.forEach((stateName) => {
-        const data = stateData[stateName] || 0;
-        updatedTotal[stateName] = data.totalCases || 0;
-        updatedNewCases[stateName] = data.newCases || 0;
-        updatedActiveCases[stateName] = data.activeCases
-        || 0;
-        updatedCriticalCases[stateName] = parseInt(data.totalCases * criticalToTotalPercentage)
-        || 0;
-        updatedVentilators[stateName] = getRandomInt(updatedCriticalCases[stateName] * 3) || 0;
-        updatedIncomingVentilators[stateName] = 0;
+      const initialData = await getInitialData(
+        criticalToTotalPercentage,
+        ventilatorToIcuPercentage,
+      );
+      const criticalProjection = {};
+      const intialStateWithExcess = new Set();
+      const initialCriticalStates = new Set();
+      stateList.forEach((stateName) => {
+        const projectedCritical = calculateProjectedCritical(initialData.criticalCases[stateName], initialData.activeCases[stateName]);
+        criticalProjection[stateName] = projectedCritical;
+        const extra = initialData.ventilators[stateName] - projectedCritical;
+        if (extra > 0) {
+          intialStateWithExcess.add(stateName);
+        } else if (extra < 0) {
+          initialCriticalStates.add(stateName);
+        }
       });
-      // UPDATING STATES
-      setTotalCases(updatedTotal);
-      setNewCases(updatedNewCases);
-      setCriticalCases(updatedCriticalCases);
-      setActiveCases(updatedActiveCases);
-      setVentilators(updatedVentilators);
-      setIncomingVentilators(updatedIncomingVentilators);
+
+      setCriticalCaseProjected(criticalProjection);
+      setTotalCases(initialData.totalCases);
+      setNewCases(initialData.newCases);
+      setCriticalCases(initialData.criticalCases);
+      setActiveCases(initialData.activeCases);
+      setVentilators(initialData.ventilators);
+      setStatesWithExcess(intialStateWithExcess);
+      setStatesInCritical(initialCriticalStates);
       setLoading(false);
     }
 
@@ -56,71 +140,126 @@ export function StateDataProvider({ children }) {
     // eslint-disable-next-line
   }, []);
 
-  // =============== SUBSCRIBERS ===============
-  const criticalCaseSubscribers = new Set();
-  const onCriticalCaseChange = (fn) => criticalCaseSubscribers.add(fn);
-  const removeOnCriticalCaseChange = (fn) => criticalCaseSubscribers.delete(fn);
-  const notifySubscribers = (targetState, prevCriticalCases, newCriticalCases) => {
-    Array.from(criticalCaseSubscribers)
-      .forEach((subscriber) => subscriber(targetState, prevCriticalCases, newCriticalCases));
-  };
+  useEffect(() => {
+    console.log(statesWithExcess);
+    console.log(statesInCritical);
+    if (statesWithExcess.size > 0 && statesInCritical.size > 0) {
+      const updatedExcess = new Set([...statesWithExcess]);
+      const updatedCritical = new Set([...statesInCritical]);
+      const updatedVentilators = { ...ventilators };
+      const updatedIncoming = { ...incomingVentilators };
+      const orderList = [];
+
+      const currentExcess = (stateName) => {
+        const excess = updatedVentilators[stateName] - criticalCaseProjected[stateName];
+        return excess > 0 ? excess : 0;
+      };
+
+      const orderVentilators = (from, to, count) => {
+        // UPDATE FROM VENTIALTORS
+        updatedVentilators[from] -= count;
+        updatedIncoming[to] += count;
+        console.log(`${from} ---> ${to} : ${count}`);
+        orderList.push(BuildOrder(from, to, count));
+      };
+
+      for (const crit of statesInCritical) {
+        // debugger;
+        let required = criticalCaseProjected[crit] - (ventilators[crit] + stateIncomingVentilators(crit));
+        console.log(required);
+        for (const excessState of updatedExcess) {
+          // debugger;
+          const excess = currentExcess(excessState);
+          if (excess === 0) {
+            updatedExcess.delete(excessState);
+          } else if (excess < required) {
+            // TAKE ALL THEIR EXTRA
+            required -= excess;
+            updatedExcess.delete(excessState);
+            // MAKE THE ORDER
+            orderVentilators(excessState, crit, excess);
+          } else if (excess >= required) {
+            // TAKE ALL THAT IS NEEDED
+            orderVentilators(excessState, crit, required);
+            // WHEN THE STATE HAS JUST ENOUGH
+            if (excess === required) updatedExcess.delete(excessState);
+            // DONE WITH CURRENT STATE
+            updatedCritical.delete(crit);
+            break;
+          }
+        }
+        // IF NO MORE EXTRA
+        if (updatedExcess.size === 0) break;
+      }
+      addBulkOrders(orderList);
+      setStatesWithExcess(updatedExcess);
+      setStatesInCritical(updatedCritical);
+      setVentilators(updatedVentilators);
+    }
+  }, [statesWithExcess, statesInCritical]);
 
   // =============== CONTEXT VALUES ===============
 
   const setStateCriticalCase = async (targetState, newCritCases) => {
-    const prevCases = criticalCases[targetState];
-    await setCriticalCases({
-      ...criticalCases,
-      [targetState]: newCritCases,
+    // const expected = criticalCaseProjected[targetState];
+    const newCrit = parseInt(newCritCases);
+    const newProjection = calculateProjectedCritical(newCrit, activeCases[targetState]);
+    setCriticalCaseProjected({
+      ...criticalCaseProjected,
+      [targetState]: newProjection,
     });
-    notifySubscribers(targetState, prevCases, newCritCases);
+
+    setCriticalCases({
+      ...criticalCases,
+      [targetState]: newCrit,
+    });
+
+    const totalVentilators = ventilators[targetState] + stateIncomingVentilators(targetState);
+    if (totalVentilators < newProjection) {
+      setStatesInCritical(new Set([...statesInCritical, targetState]));
+    } else if (totalVentilators > newProjection) {
+      setStatesWithExcess(new Set([...statesWithExcess, targetState]));
+    }
   };
 
   const setStateVentilators = (targetState, newTotal) => {
+    const total = parseInt(newTotal);
     setVentilators({
       ...ventilators,
-      [targetState]: newTotal,
+      [targetState]: total,
     });
-  };
 
-  const bulkSetStateVentilators = (list) => {
-    const newVentilatorList = { ...ventilators };
-    list.forEach(({ name, newTotal }) => {
-      newVentilatorList[name] = newTotal;
-    });
-    setVentilators(newVentilatorList);
-  };
-
-  const setStateIncomingVentilators = (targetState, newTotal) => {
-    setIncomingVentilators({
-      ...incomingVentilators,
-      [targetState]: newTotal,
-    });
+    const supplyCount = total + stateIncomingVentilators(targetState);
+    const projected = criticalCaseProjected[targetState];
+    console.log(`${supplyCount} - ${projected}`);
+    if (supplyCount < projected) {
+      const newCrit = new Set([...statesInCritical]);
+      newCrit.add(targetState);
+      setStatesInCritical(newCrit);
+    } else if (supplyCount > projected) {
+      setStatesWithExcess(new Set([...statesWithExcess, targetState]));
+    }
   };
 
   const values = {
     // DATA
-    loading,
     totalCases,
     newCases,
     criticalCases,
     activeCases,
     ventilators,
     incomingVentilators,
+    criticalCaseProjected,
     // ACTIONS
     setStateCriticalCase,
     setStateVentilators,
-    bulkSetStateVentilators,
-    setStateIncomingVentilators,
-    // LISTENERS
-    onCriticalCaseChange,
-    removeOnCriticalCaseChange,
   };
+
 
   // =============== RENDER ===============
   return (
     <StateDataContext.Provider value={values}>
-      {children}
+      {loading === true ? (<></>) : children}
     </StateDataContext.Provider>
   );
 }
