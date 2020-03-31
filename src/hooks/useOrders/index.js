d60a66459297213332d9ef2ec14b96c2d1ef3a7f
Asif Shikder <shikdernyc@gmail.com>
2020-03-31 13:56:49 -0400

    added useAllIncomingOrders

--- src/hooks/useOrders/index.js
+++ src/hooks/useOrders/index.js
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
 import OrderContext from 'contexts/Order';
-import { orderStatusActions } from 'constants/order';
+import { orderStatusActions, orderStatuses } from 'constants/order';
 import { getTransitType } from 'utils/order/getPerspectiveProps';
+import state from 'constants/state';
 
 export function useStateOrders(state) {
   const { getAllOrders } = useContext(OrderContext);
-  const stateOrders = getAllOrders().filter(order => [order.to, order.from].includes(state[1]));
+  const stateOrders = getAllOrders().filter((order) => [order.to, order.from].includes(state[1]));
   return stateOrders;
 }
 
@@ -22,7 +23,7 @@ export function useRecentOrders(count = DEFAULT_COUNT) {
 export function useRecentStateOrders(state, count = DEFAULT_COUNT) {
   const { getAllOrders } = useContext(OrderContext);
   const orders = getAllOrders()
-    .filter(order => [order.to, order.from].includes(state[1]))
+    .filter((order) => [order.to, order.from].includes(state[1]))
     .sort((a, b) => b.time_created - a.time_created)
     .slice(0, count);
   return orders;
@@ -30,8 +31,9 @@ export function useRecentStateOrders(state, count = DEFAULT_COUNT) {
 
 export function useRecentPendingStateOrders(state, count = DEFAULT_COUNT) {
   const { getAllOrders } = useContext(OrderContext);
-  const orders = getAllOrders()
-    .filter(order => {
+  const allOrders = getAllOrders();
+  const orders = allOrders
+    .filter((order) => {
       const isForState = [order.to, order.from].includes(state[1]);
       const hasAction = Boolean(orderStatusActions[order.status][getTransitType(state, order)]);
       return isForState && hasAction;
@@ -39,4 +41,28 @@ export function useRecentPendingStateOrders(state, count = DEFAULT_COUNT) {
     .sort((a, b) => b.time_created - a.time_created)
     .slice(0, count);
   return orders;
+}
+
+export function useAllIncomingOrders() {
+  const { getAllOrders } = useContext(OrderContext);
+  const orders = getAllOrders();
+  const incomingOrders = {};
+  orders.forEach(({
+    status,
+    to,
+    quantity,
+  }) => {
+    if (
+      status === orderStatuses.PENDING_SHIPMENT
+      || status === orderStatuses.IN_TRANSIT
+    ) {
+      const stateName = state[to];
+      // eslint-disable-next-line no-prototype-builtins
+      if (!incomingOrders.hasOwnProperty(stateName)) {
+        incomingOrders[stateName] = 0;
+      }
+      incomingOrders[stateName] += quantity;
+    }
+  });
+  return incomingOrders;
 }
